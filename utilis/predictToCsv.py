7ec406e315d465ffe460a9f0c0dc8f0035b2f68b
reemo <r48n34@gmail.com>
2022-03-31 16:03:43 +0800

    Update predictToCsv.py

--- utilis/predictToCsv.py
+++ utilis/predictToCsv.py
@@ -4,16 +4,19 @@ import pandas as pd
 import numpy as np 
 import PIL
 
+# model location
 model = torch.hub.load('ultralytics/yolov5', 'custom', path="G:\\yolov5Play\\yolov5\\birdPt\\best.pt")
 
 imgDir = "G:\\Data\\Test"
 imgArr = os.listdir(imgDir)
 
+# imgArr to proper img with path
 imgs = [ os.path.join(imgDir,i) for i in imgArr]
 
 model.conf = 0.20  # NMS confidence threshold
 model.iou = 0.30  # NMS IoU threshold
 
+# transfer bbox to coco format
 def calBoxPt (xmin,ymin, xmax, ymax, width, height):
     x = xmin / width
     y = ymin / height
@@ -28,17 +31,17 @@ for ind, img in enumerate(imgs):
     image = PIL.Image.open(img)
     width, height = image.size
 
-    results = model(img)
-    res = results.pandas().xyxy[0]
+    results = model(img) # predict result
+    res = results.pandas().xyxy[0] # predict result to bbox arr
 
-    if len(res.confidence) >= 1 :
+    if len(res.confidence) >= 1 : # have predict result box
 
         boxPt = calBoxPt(res.xmin[0], res.ymin[0], res.xmax[0], res.ymax[0], width, height)
-        resultStr = str(1) + " " + str(res.confidence[0]) + " " + " ".join([str(st) for st in boxPt])
+        resultStr = str(1) + " " + str(res.confidence[0]) + " " + " ".join([str(st) for st in boxPt]) # string format
 
-        finalArr.append([ imgArr[ind] ,  resultStr ]  )
+        finalArr.append([ imgArr[ind] ,  resultStr ])
     else:
-        finalArr.append([ imgArr[ind] ,  '1 0 0 0 0 0' ]  )
+        finalArr.append([ "" ])
 
 
 print(len(finalArr))
